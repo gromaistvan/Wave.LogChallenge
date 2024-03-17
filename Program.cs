3063c5d2704b253e45e4bb720f70f8f2b7987241
Groma István (SDADMS) <groma.istvandr@sdadms.hu>
2024-03-18 00:03:35 +0100

    Refactor.

--- Program.cs
+++ Program.cs
@@ -1,106 +1,119 @@
 ﻿/*
-c1 – Write a program that will print out the total number of lines in the file.
-c2 – Notice that the 8th column contains a person’s name. Write a program that loads in this data and creates an array with all name strings. Print out the 432nd and 43243rd names.
-c3 – Notice that the 5th column contains a form of date. Count how many donations occurred in each month and print out the results.
-c4 – Notice that the 8th column contains a person’s name. Create an array with each first name. Identify the most common first name in the data and how many times it occurs.
+– Write a program that will print out the total number of lines in the file.
+– Notice that the 8th column contains a person’s name. Write a program that loads in this data and creates an array with all name strings.
+  Print out the 432nd and 43243rd names.
+– Notice that the 5th column contains a form of date. Count how many donations occurred in each month and print out the results.
+– Notice that the 8th column contains a person’s name. Create an array with each first name. Identify the most common first name in
+  the data and how many times it occurs.
 */
 
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-var watch = Stopwatch.StartNew();
-await foreach (Memory<byte> line in ReadLineAsync("test.txt", 10))
 {
-    Console.WriteLine(Encoding.ASCII.GetString(line.Span));
+    var watch = Stopwatch.StartNew();
+    var (count, firstName, secoundName, eachMonth, commonName) = await ReadFile1Async("itcont.txt");
+    watch.Stop();
+    Console.WriteLine(count);
+    Console.WriteLine(firstName);
+    Console.WriteLine(secoundName);
+    Console.WriteLine(eachMonth);
+    Console.WriteLine(commonName);
+    Console.WriteLine(watch.Elapsed);
 }
-/*
-var (n, first, secound, eachMonth, maxName) = await ReadFileAsync("itcont.txt");
-watch.Stop();
-Console.WriteLine(n);
-Console.WriteLine(first);
-Console.WriteLine(secound);
-Console.WriteLine(eachMonth);
-Console.WriteLine(maxName);
-*/
-Console.WriteLine(watch.Elapsed);
+
+{
+    var watch = Stopwatch.StartNew();
+    var (count, firstName, secoundName, eachMonth, commonName) = await ReadFile2Async("itcont.txt");
+    watch.Stop();
+    Console.WriteLine(count);
+    Console.WriteLine(firstName);
+    Console.WriteLine(secoundName);
+    Console.WriteLine(eachMonth);
+    Console.WriteLine(commonName);
+    Console.WriteLine(watch.Elapsed);
+}
+
 return;
 
-#pragma warning disable CS8321 // Local function is declared but never used
-static async Task<(long count, string first, string secound, string eachMonth, string maxName)> ReadFileAsync(string fileName)
+static async Task<(long count, string firstName, string secoundName, string eachMonth, string commonName)> ReadFile1Async(string fileName, int bufferSize = 64 * 1024 * 1024, CancellationToken token = default)
 {
+    const char separator = '|';
+    var months = new long[12];
+    var count = 0L;
+    var max = 1L;
+    var names = new Dictionary<string, long>();
+    var firstName = "";
+    var secondName = "";
+    var commonName = "";
+
     using StreamReader file = new (
         fileName,
         Encoding.ASCII,
         detectEncodingFromByteOrderMarks: false,
         new FileStreamOptions
         {
-            Access = FileAccess.Read,
             Mode = FileMode.Open,
-            Options = FileOptions.SequentialScan | FileOptions.Asynchronous,
+            Access = FileAccess.Read,
             Share = FileShare.None,
-            BufferSize = 10 * 1024 * 1024
+            Options = FileOptions.Asynchronous,
+            BufferSize = bufferSize
         });
-    long n = 0, max = 1;
-    string? first = null, second = null, maxName = null;
-    var months = new int[12];
-    Dictionary<string, int> names = [];
-    while (await file.ReadLineAsync() is { } line)
+    while (await file.ReadLineAsync(token) is { } line)
     {
-        switch (++n)
-        {
-            case 432:
-                first = line;
-                break;
-            case 43243:
-                second = line;
-                break;
-        }
-        var start = 0;
-        FindPos(line, '|', 4, ref start);
-        SetMonth(ref months, line.AsSpan((start+4)..(start+6)));
-        FindPos(line, '|', 3, ref start);
+        count++;
+        int start = 0;
+        FindPos(line, 4, ref start);
+        months[(line[start + 4] - '0') * 10 + (line[start + 5] - '0') - 1]++;
+        FindPos(line, 3, ref start);
         int from = start + 1;
-        FindPos(line, ',', 1, ref start);
+        FindPos(line, 1, ref start);
         int to = start - 1;
         if (to <= from) continue;
         string name = line[from..to];
-        if (names.TryGetValue(name, out int count))
+        switch (count)
         {
-            names[name] = ++count;
-            if (count <= max) continue;
-            max = count;
-            maxName = name;
+            case 432:   firstName = name;  break;
+            case 43243: secondName = name; break;
+        }
+        if (names.TryGetValue(name, out long value))
+        {
+            names[name] = ++value;
+            if (value <= max) continue;
+            max = value;
+            commonName = name;
         }
         else
         {
-            names.Add(name, 1);
+            names.Add(name, 1L);
         }
     }
-    return (n, first ?? "", second ?? "", string.Join(',', months), maxName ?? "");
-}
-#pragma warning restore CS8321 // Local function is declared but never used
+    return (count, firstName, secondName, string.Join(',', months), commonName);
 
-static void SetMonth(ref int[] months, ReadOnlySpan<char> month) =>
-    months[(month[0] - '0') * 10 + (month[1] - '0') - 1] += 1;
-
-static void FindPos(ReadOnlySpan<char> text, char chr, int count, ref int start)
-{
-    for (var i = 0; i < count; i += 1)
+    void FindPos(ReadOnlySpan<char> text, int count, ref int start)
     {
-        var found = false;
-        for (int idx = start; idx < text.Length; idx += 1)
+        for (var i = 0; i < count; i += 1)
         {
-            if (text[idx] != chr) continue;
-            found = true;
-            start = idx + 1;
-            break;
+            var found = false;
+            for (int idx = start; idx < text.Length; idx += 1)
+            {
+                if (text[idx] != separator) continue;
+                found = true;
+                start = idx + 1;
+                break;
+            }
+            if (! found) return;
         }
-        if (! found) return;
     }
 }
 
-static async IAsyncEnumerable<Memory<byte>> ReadLineAsync(string fileName, int bufferSize = 10 * 1024 * 1024, [EnumeratorCancellation] CancellationToken token = default)
+static async IAsyncEnumerable<ReadOnlyMemory<byte>> ReadLineAsync(string fileName, int bufferSize = 64 * 1024 * 1024, [EnumeratorCancellation] CancellationToken token = default)
 {
     ArgumentException.ThrowIfNullOrEmpty(fileName);
     ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(bufferSize, 0);
@@ -112,24 +125,84 @@ static async IAsyncEnumerable<Memory<byte>> ReadLineAsync(string fileName, int b
             Mode = FileMode.Open,
             Access = FileAccess.Read,
             Share = FileShare.None,
-            Options = FileOptions.SequentialScan | FileOptions.Asynchronous,
-            BufferSize = buffer.Length
+            Options = FileOptions.Asynchronous,
+            BufferSize = bufferSize
         });
-    var start = 0;
-    while (true)
+    int start = 0, maxCount, count;
+    do
     {
-        int count = await stream.ReadAsync(buffer.AsMemory(start, buffer.Length - start), token);
-        var pos = 0;
-        for (int i = start; i < count; i += 1)
+        maxCount = bufferSize - start;
+        count = await stream.ReadAsync(buffer.AsMemory(start, maxCount), token);
+        int lastPos = 0;
+        for (int i = 0; i < count; i += 1)
         {
-            if (buffer[i] != '\n') continue;
-            yield return buffer.AsMemory(pos, i - pos);
-            pos = i + 1;
+            int pos = start + i;
+            if (buffer[pos] != '\n') continue;
+            yield return buffer.AsMemory(lastPos, pos - lastPos);
+            lastPos = pos + 1;
         }
-        Array.Copy(buffer, pos, buffer, 0, count - pos);
-        start = count - pos;
-        if (count == buffer.Length) continue;
-        yield return buffer.AsMemory(0, start);
-        yield break;
+        Array.Copy(buffer, lastPos, buffer, 0, start + count - lastPos);
+        start = start + count - lastPos;
+    }
+    while (count == maxCount);
+    if (start > 0) yield return buffer.AsMemory(0, start);
+}
+
+static async Task<(long count, string firstName, string secoundName, string eachMonth, string commonName)> ReadFile2Async(string fileName, CancellationToken token = default)
+{
+    const byte separator = (byte)'|';
+    var months = new long[12];
+    var count = 0L;
+    var max = 1L;
+    var names = new Dictionary<string, long>();
+    var firstName = "";
+    var secondName = "";
+    var commonName = "";
+
+    await foreach (ReadOnlyMemory<byte> line in ReadLineAsync(fileName).WithCancellation(token))
+    {
+        count++;
+        int start = 0;
+        FindPos(line.Span, ref start, 4);
+        months[(line.Span[start + 4] - (byte)'0') * 10 + (line.Span[start + 5] - (byte)'0') - 1]++;
+        FindPos(line.Span, ref start, 3);
+        int from = start + 1;
+        FindPos(line.Span, ref start, 1);
+        int to = start - 1;
+        if (to <= from) continue;
+        string name = Encoding.ASCII.GetString(line.Span[from..to]);
+        switch (count)
+        {
+            case 432:   firstName = name;  break;
+            case 43243: secondName = name; break;
+        }
+        if (names.TryGetValue(name, out long value))
+        {
+            names[name] = ++value;
+            if (value <= max) continue;
+            max = value;
+            commonName = name;
+        }
+        else
+        {
+            names.Add(name, 1L);
+        }
+    }
+    return (count, firstName, secondName, string.Join(',', months), commonName);
+
+    void FindPos(ReadOnlySpan<byte> text, ref int start, int count)
+    {
+        for (var i = 0; i < count; i += 1)
+        {
+            var found = false;
+            for (int idx = start; idx < text.Length; idx += 1)
+            {
+                if (text[idx] != separator) continue;
+                found = true;
+                start = idx + 1;
+                break;
+            }
+            if (! found) return;
+        }
     }
 }
